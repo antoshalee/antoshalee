adda1a4d25fa5d3a52b03b7cb226a60af218c8bf
antoshalee <antoshalee@gmail.com>
2012-08-19 18:23:26 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antoshalee::Application.config.secret_token = '689ce9b4c2e37b47bf58e7be4099dffaebf32562c2960aed74aec10a921ba491028be30b4f6b38f7eb581965b44427bf994dd6f6504fbcef9d836fd6275cba90'
